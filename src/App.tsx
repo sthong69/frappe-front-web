77e197b91d9532c4b6eea215f5cf80a583d2fc77
s22thong <sylvain.thong@gmail.com>
2024-10-22 15:13:23 +0200

    ft: added project's color palette in tailwind config

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,9 @@
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold underline text-red-600">Hello world!</h1>
+    <div className="bg-student">
+      <h1 className="text-3xl font-bold text-red-600 underline">
+        Hello world!
+      </h1>
+    </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,14 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
+    colors: {
+      student: "#E7DFD8",
+      supervisor: "#D7FFFA",
+      primary: "#38B6FF",
+      secondary: "#004AAD",
+      confirm: "#00BF63",
+      cancel: "#FF5757",
+    },
   },
   plugins: [],
 };
