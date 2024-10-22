41e578b8955a5e1996a3be1aeae0b67eda03ae2d
s22thong <sylvain.thong@gmail.com>
2024-10-22 16:08:18 +0200

    ft: replaced primary, secondary colors from shadcn with custom

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,17 @@
+import { Button } from "./components/ui/button";
+
 export default function App() {
   return (
-    <div className="bg-student flex min-h-screen items-center justify-center">
+    <div className="bg-student flex min-h-screen flex-col items-center justify-center gap-8">
       <h1 className="text-3xl font-bold text-red-500">Salut les bg</h1>
+      <Button variant={"primary"} size={"lg"}>
+        PROJET FRONT
+      </Button>
+      <Button variant={"secondary"}>Secondaire</Button>
+      <Button variant={"confirm"}>Confirmer</Button>
+      <Button variant={"cancel"} size={"sm"}>
+        Annuler
+      </Button>
     </div>
   );
 }
