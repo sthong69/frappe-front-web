42e7b7e3dd494c360d6cea8002826b53aa8bec39
s22thong <sylvain.thong@gmail.com>
2024-10-22 15:58:04 +0200

    fix: extend colors instead of replacing in tailwind config

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,7 @@
 export default function App() {
   return (
-    <div className="bg-student">
-      <h1 className="text-3xl font-bold text-red-600 underline">
-        Hello world!
-      </h1>
+    <div className="bg-student flex min-h-screen items-center justify-center">
+      <h1 className="text-3xl font-bold text-red-500">Salut les bg</h1>
     </div>
   );
 }
