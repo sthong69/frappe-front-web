8b93abf6f4efac0a69e451c869df176cadedb72c
s22thong <sylvain.thong@imt-atlantique.net>
2024-11-27 13:34:32 +0100

    fix: removed ANOTHER unused React import

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,4 +1,3 @@
-import * as React from "react";
 import { Outlet, createRootRoute } from "@tanstack/react-router";
 import Navbar from "@/components/Navbar";
 import { Toaster } from "@/components/ui/sonner";
