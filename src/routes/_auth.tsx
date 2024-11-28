dec8c2a6906c5168c4d0bb084bcdf89d3fdc251e
s22thong <sylvain.thong@imt-atlantique.net>
2024-11-28 12:43:33 +0100

    fix: remove unused console.log()

--- src/routes/_auth.tsx
+++ src/routes/_auth.tsx
@@ -2,7 +2,6 @@ import { createFileRoute, Outlet, redirect } from "@tanstack/react-router";
 
 export const Route = createFileRoute("/_auth")({
   beforeLoad: ({ context, location }) => {
-    console.log(context);
     if (!context.auth.isLoaded) {
       throw redirect({
         to: "/",
