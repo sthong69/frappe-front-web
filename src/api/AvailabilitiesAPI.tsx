da7086866792b4ce274e66fc2727410b2bb030a7
s22thong <sylvain.thong@imt-atlantique.net>
2025-01-13 16:09:45 +0100

    ft: revamped availabilitiesAPI with new backend endpoints

--- src/api/AvailabilitiesAPI.tsx
+++ src/api/AvailabilitiesAPI.tsx
@@ -1,23 +1,28 @@
 import { publicAPI } from "./axios";
-import { getHours, getMinutes, setHours, setMinutes } from "date-fns";
+import {
+  getHours,
+  getMinutes,
+  setHours,
+  setMinutes,
+  getYear,
+  getMonth,
+  getDate,
+} from "date-fns";
 
 export const getAvailableDays = async (meetingInfos: {
   supervisorId: number;
   duration: string;
 }): Promise<Date[]> => {
   return publicAPI
-    .get("/availabilities", {
+    .get(`/availabilities/${meetingInfos.supervisorId}/days`, {
       params: {
-        supervisorid: meetingInfos.supervisorId,
         duration: meetingInfos.duration,
       },
     })
-    .then(function (response: {
-      data: { start: string; end: string; duration: string }[];
-    }) {
+    .then(function (response: { data: string[] }) {
       let parsedData = response.data.map((item) => {
-        const date = setHours(setMinutes(new Date(item.start), 0), 0);
-        return date;
+        let parsedDate = new Date(item);
+        return setHours(setMinutes(parsedDate, 0), 0);
       });
       return Promise.resolve(parsedData);
     })
@@ -26,37 +31,32 @@ export const getAvailableDays = async (meetingInfos: {
     });
 };
 
-export const getAvailableHours = async (meetingInfos: {
+export const getAvailableSlots = async (meetingInfos: {
   supervisorId: number;
   day: Date;
   duration: string;
 }): Promise<
   {
-    hours: number;
-    minutes: number;
-    duration: string;
+    startHours: number;
+    startMinutes: number;
   }[]
 > => {
   let parsedDate = new Date(meetingInfos.day);
-  console.log(parsedDate);
-  let formattedDate = `${parsedDate.getFullYear()}-${parsedDate.getMonth() + 1}-${parsedDate.getDate()}`;
-  console.log(formattedDate);
+  let formattedDate = `${getYear(parsedDate)}-${getMonth(parsedDate) + 1}-${getDate(parsedDate)}`;
   return publicAPI
-    .get("/dailyavailabilities", {
+    .get(`/availabilities/${meetingInfos.supervisorId}/slots`, {
       params: {
-        supervisorid: meetingInfos.supervisorId,
         date: formattedDate,
         duration: meetingInfos.duration,
       },
     })
     .then(function (response: {
-      data: { start: string; end: string; duration: string }[];
+      data: { start: string; end: string; duration: string; remote: boolean }[];
     }) {
       console.log(response.data);
       let parsedData = response.data.map((item) => ({
-        hours: getHours(new Date(item.start)),
-        minutes: getMinutes(new Date(item.start)),
-        duration: item.duration,
+        startHours: getHours(new Date(item.start)),
+        startMinutes: getMinutes(new Date(item.start)),
       }));
       return Promise.resolve(parsedData);
     })
