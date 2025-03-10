5a8a3ff29107c0e5838c62757da6a5c03b7eba45
s22thong <sylvain.thong@imt-atlantique.net>
2025-03-10 13:17:36 +0100

    ft(DX): same

--- src/router.tsx
+++ src/router.tsx
@@ -2,13 +2,10 @@ import {
   createBrowserHistory,
   createHashHistory,
   createRouter,
-  Link,
 } from "@tanstack/react-router";
 
 import { routeTree } from "./routeTree.gen";
-import Page from "./components/Page";
-import { Button } from "./components/ui/button";
-import { ArrowLeft } from "lucide-react";
+import PageNotFound from "./components/PageNotFound";
 
 const history = import.meta.env.DEV
   ? createBrowserHistory()
@@ -21,41 +18,6 @@ export const router = createRouter({
   },
   history,
   defaultNotFoundComponent: () => {
-    return (
-      <Page className="flex flex-1 flex-col">
-        <div className="flex h-full w-full flex-grow items-center justify-center">
-          <div className="mx-auto max-w-md space-y-6">
-            <div className="relative mx-auto h-60 w-60">
-              <div className="absolute inset-0 flex items-center justify-center">
-                <div className="text-[180px] font-bold text-muted-foreground/20">
-                  404
-                </div>
-              </div>
-            </div>
-
-            <div className="space-y-2">
-              <h1 className="text-center text-3xl font-bold tracking-tighter sm:text-4xl">
-                Page non trouvée
-              </h1>
-              <p className="text-center text-muted-foreground">
-                Désolé, nous n'avons pas pu trouver la page que vous recherchez.
-                Il se peut qu'elle ait été déplacée, supprimée ou qu'elle n'ait
-                jamais existé.
-              </p>
-            </div>
-
-            {/* Action Buttons */}
-            <div className="flex flex-col gap-2 sm:flex-row sm:justify-center">
-              <Link to="/">
-                <Button size="lg" className="gap-1 font-bold">
-                  <ArrowLeft className="h-4 w-4" />
-                  Revenir à l'accueil
-                </Button>
-              </Link>
-            </div>
-          </div>
-        </div>
-      </Page>
-    );
+    return <PageNotFound />;
   },
 });
